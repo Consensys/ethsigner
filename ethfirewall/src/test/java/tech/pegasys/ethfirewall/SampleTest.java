3f2902142bce6463f7ff5b38437b5d6259d9f5cd
tmohay <trent.mohay@consensys.net>
2019-03-01 16:25:08 +1100

    Added unit test

---
+++ ethfirewall/src/test/java/tech/pegasys/ethfirewall/SampleTest.java
@@ -0,0 +1,14 @@
+package tech.pegasys.ethfirewall;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+
+public class SampleTest {
+
+  @Test
+  public void passingTest() {
+    assertThat(true).isTrue();
+  }
+
+}
