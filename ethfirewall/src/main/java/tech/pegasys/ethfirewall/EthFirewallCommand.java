82823fd142648c406f672e40aa05002ab20c4216
tmohay <trent.mohay@consensys.net>
2019-03-04 19:10:32 +1100

    trying to beat the licencing

--- ethfirewall/src/main/java/tech/pegasys/ethfirewall/EthFirewallCommand.java
+++ ethfirewall/src/main/java/tech/pegasys/ethfirewall/EthFirewallCommand.java
@@ -1,17 +1,28 @@
+/*
+ * Copyright 2019 ConsenSys AG.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
 package tech.pegasys.ethfirewall;
 
 import java.io.File;
 import java.io.PrintStream;
-import java.util.List;
+
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.core.config.Configurator;
 import picocli.CommandLine;
-import picocli.CommandLine.AbstractParseResultHandler;
 import picocli.CommandLine.Command;
-import picocli.CommandLine.DefaultExceptionHandler;
 import picocli.CommandLine.Option;
+import picocli.CommandLine.ParameterException;
 
 @SuppressWarnings("FieldCanBeLocal") // because Picocli injected fields report false positives
 @Command(
@@ -25,8 +36,7 @@ import picocli.CommandLine.Option;
     descriptionHeading = "%nDescription:%n%n",
     optionListHeading = "%nOptions:%n",
     footerHeading = "%n",
-    footer = "Ethfirewall is licensed under the Apache License 2.0"
-)
+    footer = "Ethfirewall is licensed under the Apache License 2.0")
 public class EthFirewallCommand implements Runnable {
 
   private static final Logger LOG = LogManager.getLogger();
@@ -37,76 +47,73 @@ public class EthFirewallCommand implements Runnable {
       paramLabel = "<LOG VERBOSITY LEVEL>",
       description =
           "Logging verbosity levels: OFF, FATAL, WARN, INFO, DEBUG, TRACE, ALL (default: INFO)")
-  private final Level logLevel = null;
+  private final Level logLevel = Level.INFO;
 
   @Option(
       names = {"-p", "--password"},
       description = "Password required to access the key file.",
       required = true,
       arity = "1")
-  private String password = null;
+  private String password;
 
+  @SuppressWarnings("FieldMayBeFinal") // Because PicoCLI requires Strings to not be final.
   @Option(
       names = {"-k", "--keyfile"},
       description = "The path to a file containing the key used to sign transactions.",
       required = true,
       arity = "1")
-  private final File keyFilename = null;
+  private File keyFilename;
 
   @Option(
       names = "--downstream-host",
       description = "The endpoint to which received requests are forwarded",
-      required = true,
       arity = "1")
-  private String web3EndpointHost;
+  private String downstreamHost = "127.0.0.1";
 
   @Option(
       names = "--downstream-port",
       description = "The endpoint to which received requests are forwarded",
-      required = true,
       arity = "1")
-  private String web3EndpointPort;
+  private Integer downstreamPort;
 
   @SuppressWarnings("FieldMayBeFinal") // Because PicoCLI requires Strings to not be final.
   @Option(
       names = {"--rpc-listen-host"},
       description = "Host for JSON-RPC HTTP to listen on (default: ${DEFAULT-VALUE})",
-      required = true,
       arity = "1")
   private String listenHost = "127.0.0.1";
 
   @Option(
       names = {"--rpc-listen-port"},
       description = "Port for JSON-RPC HTTP to listen on (default: ${DEFAULT-VALUE})",
-      required = true,
       arity = "1")
   private final Integer listenPort = 8545;
 
-  public void parse(
-      final PrintStream output,
-      final String... args) {
+  public void parse(final PrintStream output, final String... args) {
 
     commandLine = new CommandLine(this);
     commandLine.setCaseInsensitiveEnumValuesAllowed(true);
 
     // Must manually show the usage/version info, as per the design of picocli
     // (https://picocli.info/#_printing_help_automatically)
-    commandLine.parse(args);
-    if (commandLine.isUsageHelpRequested()) {
-      commandLine.usage(output);
-      return;
-    } else if (commandLine.isVersionHelpRequested()) {
-      commandLine.printVersionHelp(System.out);
-      return;
+    try {
+      commandLine.parse(args);
+      if (commandLine.isUsageHelpRequested()) {
+        commandLine.usage(output);
+        return;
+      } else if (commandLine.isVersionHelpRequested()) {
+        commandLine.printVersionHelp(System.out);
+        return;
+      }
+    } catch (ParameterException ex) {
+      output.println(ex.getMessage());
     }
   }
 
   @Override
   public void run() {
     // set log level per CLI flags
-    if (logLevel != null) {
-      System.out.println("Setting logging level to " + logLevel.name());
-      Configurator.setAllLevels("", logLevel);
-    }
+    System.out.println("Setting logging level to " + logLevel.name());
+    Configurator.setAllLevels("", logLevel);
   }
 }
--- ethfirewall/src/test/java/tech/pegasys/ethfirewall/EthFirewallCommandTest.java
+++ ethfirewall/src/test/java/tech/pegasys/ethfirewall/EthFirewallCommandTest.java
@@ -1,20 +1,27 @@
+/*
+ * Copyright 2019 ConsenSys AG.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
 package tech.pegasys.ethfirewall;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static picocli.CommandLine.defaultExceptionHandler;
 
-import com.sun.org.apache.bcel.internal.generic.ANEWARRAY;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
-import java.util.List;
+
 import org.apache.logging.log4j.Logger;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
-import picocli.CommandLine.DefaultExceptionHandler;
-import picocli.CommandLine.Help.Ansi;
-import picocli.CommandLine.RunLast;
 
 @RunWith(MockitoJUnitRunner.class)
 public class EthFirewallCommandTest {
@@ -29,9 +36,7 @@ public class EthFirewallCommandTest {
   final EthFirewallCommand command = new EthFirewallCommand();
 
   private void parseCommand(String... args) {
-    command.parse(
-        outPrintStream,
-        args);
+    command.parse(outPrintStream, args);
   }
 
   @Test
@@ -43,7 +48,17 @@ public class EthFirewallCommandTest {
   }
 
   @Test
-  public void callingVersionDisplaysEthFireWallVersionString() {
+  public void invalidInputForPortShowsError() {
+    parseCommand(
+        "--password=MyPassword --keyfile=./keyfile --downstream-host=127.0.0.1 --downstream-port=abc"
+            .split(" "));
+    assertThat(commandOutput.toString()).contains("--downstream-port", "'abc' is not an int");
+  }
 
+  @Test
+  public void missingPasswordShowsError() {
+    parseCommand(
+        "--keyfile=./keyfile --downstream-host=127.0.0.1 --downstream-port=5000".split(" "));
+    assertThat(commandOutput.toString()).contains("--password", "Missing");
   }
 }
