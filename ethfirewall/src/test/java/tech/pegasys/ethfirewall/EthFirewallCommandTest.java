8069846ec4c44f292b2104d88b9927d58a0968fc
tmohay <trent.mohay@consensys.net>
2019-03-07 14:12:54 +1100

    fix spotless

--- ethfirewall/src/test/java/tech/pegasys/ethfirewall/EthFirewallCommandTest.java
+++ ethfirewall/src/test/java/tech/pegasys/ethfirewall/EthFirewallCommandTest.java
@@ -67,13 +67,14 @@ public class EthFirewallCommandTest {
 
   @Test
   public void fieldsFromCommandLineAreStoredInVariables() {
-    parseCommand("--keyfile=./keyfile "
-        + "--downstream-http-host=127.0.0.1 "
-        + "--downstream-http-port=5000 "
-        + "--http-listen-port=5001"
-        + "--http-listen-host=localhost "
-        + "--logging=TRACE");
+    parseCommand(
+        "--keyfile=./keyfile "
+            + "--downstream-http-host=127.0.0.1 "
+            + "--downstream-http-port=5000 "
+            + "--http-listen-port=5001"
+            + "--http-listen-host=localhost "
+            + "--logging=TRACE");
 
-    //TODO add these tests when Runner is in place.0
+    // TODO add these tests when Runner is in place.0
   }
 }
