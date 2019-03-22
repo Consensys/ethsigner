7f8229bd5c8d8eac1c13ab508e69e320b9e2478a
tmohay <trent.mohay@consensys.net>
2019-03-22 15:14:16 +1100

    Take an long chainID

--- ethfirewall/src/main/java/tech/pegasys/ethfirewall/signing/ChainIdProvider.java
+++ ethfirewall/src/main/java/tech/pegasys/ethfirewall/signing/ChainIdProvider.java
@@ -15,5 +15,5 @@ package tech.pegasys.ethfirewall.signing;
 /** Obtains the chain ID for the blockchain that transaction are being signed. */
 public interface ChainIdProvider {
 
-  byte id();
+  long id();
 }
