566a5eaf7e7747dd23a38f8f050cddeb39b04c16
CjHare <CjHare@users.noreply.github.com>
2019-03-27 08:53:32 +1000

    refactoring IT

--- ethfirewall/src/integration-test/java/tech/pegasys/ethfirewall/jsonrpcproxy/SigningSendTransactionTest.java
+++ ethfirewall/src/integration-test/java/tech/pegasys/ethfirewall/jsonrpcproxy/SigningSendTransactionTest.java
@@ -39,30 +39,15 @@ public class SigningSendTransactionTest extends IntegrationTestBase {
 
   @Test
   public void signSendTransaction() throws Exception {
-    final Transaction transaction =
-        new Transaction(
-            "0xb60e8dd61c5d32be8058bb8eb970870f07233155",
-            new BigInteger(
-                "101454411220705080123888225389655371100299455501706857686025051036223022797554"),
-            new BigInteger("10000000000000"),
-            new BigInteger("30400"),
-            "0xd46e8dd67c5d32be8058bb8eb970870f07244567",
-            new BigInteger("2441406250"),
-            "0xd46e8dd67c5d32be8d46e8dd67c5d32be8058bb8eb970870f072445675058bb8eb970870f072445675");
-
-    // we create the eth_sendRawTransaction req with same id as the eth_sendTransaction req
-    final long transactionId = 5;
 
     final Request<?, ? extends Response<?>> sendTransactionRequest =
-        sendTransactionRequest(transactionId, transaction);
+        defaultSendTransactionRequest();
 
     final Request<?, ? extends Response<?>> sendRawTransactionRequest =
-        sendRawTransactionRequest(
-            transactionId,
-            "0xf8b2a0e04d296d2460cfb8472af2c5fd05b5a214109c25688d3704aed5484f9a7792f28609184e72a0008276c094d46e8dd67c5d32be8058bb8eb970870f07244567849184e72aa9d46e8dd67c5d32be8d46e8dd67c5d32be8058bb8eb970870f072445675058bb8eb970870f07244567535a0f04e0e7b41adea417596550611138a3ec9a452abb6648d734107c53476e76a27a05b826d9e9b4e0dd0e7b8939c102a2079d71cfc27cd6b7bebe5a006d5ad17d780");
+        defaultSendRawTransactionRequest();
 
     final Response<String> sendRawTransactionResponse =
-        sendRawTransactionResponse(
+        createSendRawTransactionResponse(
             "0xe670ec64341771606e55d6b4ca35a1a6b75ee3d5145a99d05921026d1527331");
 
     setUpEthereumNodeResponse(
@@ -78,8 +63,59 @@ public class SigningSendTransactionTest extends IntegrationTestBase {
     verifyEthereumNodeReceived(sendRawTransactionRequest);
   }
 
+  // TODO refacotr below methods into utility (after complete tests)
+
+  /**
+   * Due to the underlying server mocking, When only a single request is used, the contents does not
+   * actually matter, only their equivalence does.
+   */
+  private Request<?, ? extends Response<?>> defaultSendTransactionRequest() {
+    final Transaction transaction =
+        new Transaction(
+            "0xb60e8dd61c5d32be8058bb8eb970870f07233155",
+            new BigInteger(
+                "101454411220705080123888225389655371100299455501706857686025051036223022797554"),
+            new BigInteger("10000000000000"),
+            new BigInteger("30400"),
+            "0xd46e8dd67c5d32be8058bb8eb970870f07244567",
+            new BigInteger("2441406250"),
+            "0xd46e8dd67c5d32be8d46e8dd67c5d32be8058bb8eb970870f072445675058bb8eb970870f072445675");
+
+    final Request<?, ? extends Response<?>> sendTransactionRequest =
+        jsonRpc.ethSendTransaction(transaction);
+    sendTransactionRequest.setId(77);
+    return sendTransactionRequest;
+  }
+
+  private Request<?, ? extends Response<?>> defaultSendRawTransactionRequest() {
+    final Request<?, ? extends Response<?>> sendRawTransactionRequest =
+        jsonRpc.ethSendRawTransaction(
+            "0xf8b2a0e04d296d2460cfb8472af2c5fd05b5a214109c25688d3704aed5484f9a7792f28609184e72a0008276c094d46e8dd67c5d32be8058bb8eb970870f07244567849184e72aa9d46e8dd67c5d32be8d46e8dd67c5d32be8058bb8eb970870f072445675058bb8eb970870f07244567535a0f04e0e7b41adea417596550611138a3ec9a452abb6648d734107c53476e76a27a05b826d9e9b4e0dd0e7b8939c102a2079d71cfc27cd6b7bebe5a006d5ad17d780");
+    sendRawTransactionRequest.setId(77);
+
+    return sendRawTransactionRequest;
+  }
+
   private Request<?, ? extends Response<?>> sendTransactionRequest(
-      final long id, final Transaction transaction) {
+      final String from,
+      final String to,
+      final String nonce,
+      final String gas,
+      final String gasPrice,
+      final String value,
+      final String data,
+      final long id) {
+
+    final Transaction transaction =
+        new Transaction(
+            from,
+            new BigInteger(nonce),
+            new BigInteger(gasPrice),
+            new BigInteger(gas),
+            to,
+            new BigInteger(value),
+            data);
+
     final Request<?, ? extends Response<?>> sendTransactionRequest =
         jsonRpc.ethSendTransaction(transaction);
     sendTransactionRequest.setId(id);
@@ -95,7 +131,7 @@ public class SigningSendTransactionTest extends IntegrationTestBase {
     return sendRawTransactionRequest;
   }
 
-  private Response<String> sendRawTransactionResponse(final String value) {
+  private Response<String> createSendRawTransactionResponse(final String value) {
     final Response<String> sendRawTransactionResponse = new EthSendTransaction();
     sendRawTransactionResponse.setResult(value);
     return sendRawTransactionResponse;
