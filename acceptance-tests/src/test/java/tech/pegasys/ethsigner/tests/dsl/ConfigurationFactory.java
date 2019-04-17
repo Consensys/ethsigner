a6cca1ecb04534963221223f8bed6e8696ffd26d
CjHare <CjHare@users.noreply.github.com>
2019-04-17 14:35:21 +1000

    More time

--- acceptance-tests/src/test/java/tech/pegasys/ethsigner/tests/dsl/ConfigurationFactory.java
+++ acceptance-tests/src/test/java/tech/pegasys/ethsigner/tests/dsl/ConfigurationFactory.java
@@ -54,8 +54,8 @@ public class ConfigurationFactory {
   private DockerClient createDockerClient(final DefaultDockerClientConfig config) {
     final DockerCmdExecFactory dockerCmdExecFactory =
         new JerseyDockerCmdExecFactory()
-            .withReadTimeout(5000)
-            .withConnectTimeout(5000)
+            .withReadTimeout(7500)
+            .withConnectTimeout(7500)
             .withMaxTotalConnections(100)
             .withMaxPerRouteConnections(10);
 
