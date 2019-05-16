1071352b4a8ef927f35fde97c37329ebdd104eac
tmohay <trent.mohay@consensys.net>
2019-05-16 16:07:42 +1000

    moved over integration tests

---
+++ ethsigner/core/src/integration-test/java/tech/pegasys/ethsigner/jsonrpcproxy/model/jsonrpc/SendRawTransaction.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2019 ConsenSys AG.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package tech.pegasys.ethsigner.jsonrpcproxy.model.jsonrpc;
+
+import io.vertx.core.json.Json;
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.Request;
+import org.web3j.protocol.core.Response;
+import org.web3j.protocol.core.methods.response.EthSendTransaction;
+
+public class SendRawTransaction {
+
+  private final Web3j jsonRpc;
+
+  public SendRawTransaction(final Web3j jsonRpc) {
+    this.jsonRpc = jsonRpc;
+  }
+
+  public String request() {
+    final Request<?, ? extends Response<?>> sendRawTransactionRequest =
+        jsonRpc.ethSendRawTransaction(
+            "0xf8b2a0e04d296d2460cfb8472af2c5fd05b5a214109c25688d3704aed5484f9a7792f28609184e72a0008276c094d46e8dd67c5d32be8058bb8eb970870f07244567849184e72aa9d46e8dd67c5d32be8d46e8dd67c5d32be8058bb8eb970870f072445675058bb8eb970870f07244567535a0f04e0e7b41adea417596550611138a3ec9a452abb6648d734107c53476e76a27a05b826d9e9b4e0dd0e7b8939c102a2079d71cfc27cd6b7bebe5a006d5ad17d780");
+    sendRawTransactionRequest.setId(77);
+
+    return Json.encode(sendRawTransactionRequest);
+  }
+
+  public String request(final String value) {
+    final Request<?, ? extends Response<?>> sendRawTransactionRequest =
+        jsonRpc.ethSendRawTransaction(value);
+    sendRawTransactionRequest.setId(77);
+
+    return Json.encode(sendRawTransactionRequest);
+  }
+
+  public String response(final String value) {
+    final Response<String> sendRawTransactionResponse = new EthSendTransaction();
+    sendRawTransactionResponse.setResult(value);
+    return Json.encode(sendRawTransactionResponse);
+  }
+}
---
+++ ethsigner/core/src/integration-test/java/tech/pegasys/ethsigner/jsonrpcproxy/model/response/EthResponseFactory.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2019 ConsenSys AG.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package tech.pegasys.ethsigner.jsonrpcproxy.model.response;
+
+import static java.util.Collections.emptyMap;
+
+import tech.pegasys.ethsigner.jsonrpc.response.JsonRpcError;
+import tech.pegasys.ethsigner.jsonrpc.response.JsonRpcErrorResponse;
+
+import java.util.Map;
+
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.vertx.core.json.Json;
+
+public class EthResponseFactory {
+
+  private static final Map<String, String> NO_HEADERS = emptyMap();
+  private static final int DEFAULT_ID = 77;
+
+  public EthSignerResponse ethSigner(final Object id, final JsonRpcError error) {
+    return new EthSignerResponse(
+        NO_HEADERS, new JsonRpcErrorResponse(id, error), HttpResponseStatus.BAD_REQUEST);
+  }
+
+  public EthSignerResponse ethSigner(final JsonRpcError error) {
+    return new EthSignerResponse(
+        NO_HEADERS, new JsonRpcErrorResponse(DEFAULT_ID, error), HttpResponseStatus.BAD_REQUEST);
+  }
+
+  public EthSignerResponse ethSigner(final Map<String, String> headers, final String body) {
+    return new EthSignerResponse(headers, body, HttpResponseStatus.OK);
+  }
+
+  public EthSignerResponse ethSigner(final String body) {
+    return new EthSignerResponse(NO_HEADERS, body, HttpResponseStatus.OK);
+  }
+
+  public EthSignerResponse ethSigner(final String body, final HttpResponseStatus code) {
+    return new EthSignerResponse(NO_HEADERS, body, code);
+  }
+
+  public EthNodeResponse ethNode(final Map<String, String> headers, final String body) {
+    return new EthNodeResponse(headers, body, HttpResponseStatus.OK);
+  }
+
+  public EthNodeResponse ethNode(final String body) {
+    return new EthNodeResponse(NO_HEADERS, body, HttpResponseStatus.OK);
+  }
+
+  public EthNodeResponse ethNode(final String body, final HttpResponseStatus code) {
+    return new EthNodeResponse(NO_HEADERS, body, code);
+  }
+
+  public EthNodeResponse ethNode(final JsonRpcError error) {
+    final JsonRpcErrorResponse errorResponse = new JsonRpcErrorResponse(DEFAULT_ID, error);
+
+    return new EthNodeResponse(
+        NO_HEADERS, Json.encode(errorResponse), HttpResponseStatus.BAD_REQUEST);
+  }
+}
