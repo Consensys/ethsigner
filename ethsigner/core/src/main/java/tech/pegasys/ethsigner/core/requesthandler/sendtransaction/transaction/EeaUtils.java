6dc2ec2866044675e5eb29a0a47b8db68885dd8e
tmohay <trent.mohay@consensys.net>
2019-07-23 14:02:22 +1000

    fix spotless

--- ethsigner/core/src/main/java/tech/pegasys/ethsigner/core/requesthandler/sendtransaction/transaction/EeaUtils.java
+++ ethsigner/core/src/main/java/tech/pegasys/ethsigner/core/requesthandler/sendtransaction/transaction/EeaUtils.java
@@ -18,6 +18,7 @@ import java.util.Base64;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
+
 import org.web3j.crypto.Hash;
 import org.web3j.rlp.RlpEncoder;
 import org.web3j.rlp.RlpList;
