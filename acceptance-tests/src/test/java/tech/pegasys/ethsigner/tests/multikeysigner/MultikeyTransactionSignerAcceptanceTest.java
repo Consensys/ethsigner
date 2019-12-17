d07b523a6096a1004aa6a40de400c769490b7a43
Trent Mohay <trent.mohay@consensys.net>
2019-12-17 11:50:13 +1100

    added signing test for azure multikey

---
+++ acceptance-tests/src/test/java/tech/pegasys/ethsigner/tests/multikeysigner/MultikeyTransactionSignerAcceptanceTest.java
@@ -0,0 +1,115 @@
+/*
+ * Copyright ConsenSys AG.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package tech.pegasys.ethsigner.tests.multikeysigner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static tech.pegasys.ethsigner.tests.dsl.Gas.GAS_PRICE;
+import static tech.pegasys.ethsigner.tests.dsl.Gas.INTRINSIC_GAS;
+
+import com.github.dockerjava.api.DockerClient;
+import java.math.BigInteger;
+import java.nio.file.Path;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assumptions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+import org.web3j.protocol.core.methods.request.Transaction;
+import org.web3j.utils.Convert;
+import org.web3j.utils.Convert.Unit;
+import tech.pegasys.ethsigner.tests.AcceptanceTestBase;
+import tech.pegasys.ethsigner.tests.dsl.Account;
+import tech.pegasys.ethsigner.tests.dsl.DockerClientFactory;
+import tech.pegasys.ethsigner.tests.dsl.node.BesuNode;
+import tech.pegasys.ethsigner.tests.dsl.node.Node;
+import tech.pegasys.ethsigner.tests.dsl.node.NodeConfiguration;
+import tech.pegasys.ethsigner.tests.dsl.node.NodeConfigurationBuilder;
+import tech.pegasys.ethsigner.tests.dsl.signer.Signer;
+import tech.pegasys.ethsigner.tests.dsl.signer.SignerConfiguration;
+import tech.pegasys.ethsigner.tests.dsl.signer.SignerConfigurationBuilder;
+
+public class MultikeyTransactionSignerAcceptanceTest extends MultiKeyAcceptanceTestBase {
+
+  private static Node ethNode;
+  private static Signer ethSigner;
+  private static final String RECIPIENT = "0x1b00ba00ca00bb00aa00bc00be00ac00ca00da00";
+  static final String clientId = System.getenv("ETHSIGNER_AZURE_CLIENT_ID");
+  static final String clientSecret = System.getenv("ETHSIGNER_AZURE_CLIENT_SECRET");
+  static final String FILENAME = "fe3b557e8fb62b89f4916b721be55ceb828dbd73";
+
+
+  protected Account richBenefactor() {
+    return ethSigner.accounts().richBenefactor();
+  }
+
+  private static void setUpBase(final Path tomlDirectory) {
+    Assumptions.assumeTrue(
+        clientId != null && clientSecret != null,
+        "Ensure Azure client id and client secret env variables are set");
+
+    Runtime.getRuntime().addShutdownHook(new Thread(AcceptanceTestBase::tearDownBase));
+
+    final DockerClient docker = new DockerClientFactory().create();
+    final NodeConfiguration nodeConfig = new NodeConfigurationBuilder().build();
+
+    ethNode = new BesuNode(docker, nodeConfig);
+    ethNode.start();
+    ethNode.awaitStartupCompletion();
+
+    final SignerConfiguration signerConfig =
+        new SignerConfigurationBuilder().withMultiKeySignerDirectory(tomlDirectory).build();
+
+    ethSigner = new Signer(signerConfig, nodeConfig, ethNode.ports());
+    ethSigner.start();
+    ethSigner.awaitStartupCompletion();
+  }
+
+  @AfterAll
+  public static void tearDownBase() {
+    if (ethNode != null) {
+      ethNode.shutdown();
+    }
+
+    if (ethSigner != null) {
+      ethSigner.shutdown();
+    }
+  }
+
+  @Test
+  public void azureLoadedFromMultikeyCanSignValueTransferTransaction(@TempDir Path tomlDirectory) {
+    createAzureTomlFileAt(
+        "arbitrary_prefix" + FILENAME + ".toml", clientId, clientSecret, tomlDirectory);
+
+    setUpBase(tomlDirectory);
+    final BigInteger transferAmountWei = Convert.toWei("1.75", Unit.ETHER).toBigIntegerExact();
+
+    final BigInteger startBalance = ethNode.accounts().balance(RECIPIENT);
+    final Transaction transaction =
+        Transaction.createEtherTransaction(
+            richBenefactor().address(),
+            null,
+            GAS_PRICE,
+            INTRINSIC_GAS,
+            RECIPIENT,
+            transferAmountWei);
+
+    final String hash = ethSigner.transactions().submit(transaction);
+    ethNode.transactions().awaitBlockContaining(hash);
+
+    final BigInteger expectedEndBalance = startBalance.add(transferAmountWei);
+    final BigInteger actualEndBalance = ethNode.accounts().balance(RECIPIENT);
+    assertThat(actualEndBalance).isEqualTo(expectedEndBalance);
+  }
+
+}
