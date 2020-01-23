e8663c38037ec84dcfba54ac264db8cdad576897
Usman Saleem <usman@usmans.info>
2020-01-24 09:37:10 +1000

    spotless fix
    Signed-off-by: Usman Saleem <usman@usmans.info>

--- ethsigner/signer/hashicorp/src/main/java/tech/pegasys/ethsigner/signer/hashicorp/HashicorpVaultSignerFactory.java
+++ ethsigner/signer/hashicorp/src/main/java/tech/pegasys/ethsigner/signer/hashicorp/HashicorpVaultSignerFactory.java
@@ -19,8 +19,6 @@ import tech.pegasys.ethsigner.signer.filebased.CredentialTransactionSigner;
 import java.util.Map;
 
 import io.vertx.core.json.JsonObject;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.web3j.crypto.Credentials;
 
 public class HashicorpVaultSignerFactory {
