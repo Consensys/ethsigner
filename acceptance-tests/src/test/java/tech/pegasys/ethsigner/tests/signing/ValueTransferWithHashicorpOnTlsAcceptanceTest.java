bc9da3d27c6696cca1fe74a6541945d2487837df
Trent Mohay <37158202+rain-on@users.noreply.github.com>
2020-02-17 11:02:20 +1100

    Close Vertx on initialisation failures and resolve test cleanup issues
    Ethsigner was experiencing instability during acceptance testing on CircleCI.
    
    This instability was identified to originate from:
    * Vertx remaining "open" on certain TLS initialisation failures (thus Ethsigner would not exit)
    * Test clean up not always being invoked, or being invoked multiple times.

--- acceptance-tests/src/test/java/tech/pegasys/ethsigner/tests/signing/ValueTransferWithHashicorpOnTlsAcceptanceTest.java
+++ acceptance-tests/src/test/java/tech/pegasys/ethsigner/tests/signing/ValueTransferWithHashicorpOnTlsAcceptanceTest.java
@@ -66,7 +66,7 @@ public class ValueTransferWithHashicorpOnTlsAcceptanceTest {
     ethSigner.awaitStartupCompletion();
   }
 
-  static void tearDownBase() {
+  static synchronized void tearDownBase() {
     if (ethNode != null) {
       ethNode.shutdown();
       ethNode = null;
