ec403cccb33b3b26f1fb1f6f83cafeece1e30f3a
Trent Mohay <trent.mohay@consensys.net>
2020-03-10 23:22:54 +1100

    move to int test

--- ethsigner/core/src/integration-test/java/tech/pegasys/ethsigner/jsonrpcproxy/IllegalSignatureCreationTest.java
+++ ethsigner/core/src/integration-test/java/tech/pegasys/ethsigner/jsonrpcproxy/IllegalSignatureCreationTest.java
@@ -1,5 +1,5 @@
-package tech.pegasys.ethsigner.signer.filebased;/*
- * Copyright ConsenSys AG.
+/*
+ * Copyright 2020 ConsenSys AG.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
@@ -9,25 +9,20 @@ package tech.pegasys.ethsigner.signer.filebased;/*
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
- *
- * SPDX-License-Identifier: Apache-2.0
  */
+package tech.pegasys.ethsigner.jsonrpcproxy;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
-import java.math.BigInteger;
-import org.junit.jupiter.api.Test;
-import org.web3j.crypto.Credentials;
-import org.web3j.crypto.RawTransaction;
-import org.web3j.crypto.TransactionEncoder;
 import tech.pegasys.ethsigner.core.jsonrpc.EthSendTransactionJsonParameters;
-import tech.pegasys.ethsigner.core.jsonrpc.JsonRpcRequestId;
 import tech.pegasys.ethsigner.core.requesthandler.sendtransaction.transaction.EthTransaction;
 import tech.pegasys.ethsigner.core.signing.Signature;
-import tech.pegasys.ethsigner.core.signing.TransactionSerializer;
+import tech.pegasys.ethsigner.signer.filebased.CredentialTransactionSigner;
 
-class CredentialTransactionSignerTest {
+import org.junit.jupiter.api.Test;
+import org.web3j.crypto.Credentials;
+
+class IllegalSignatureCreationTest {
 
   @Test
   void ensureZenDesk32060IsResolved() {
@@ -42,17 +37,16 @@ class CredentialTransactionSignerTest {
     txnParams.data("0x0");
     txnParams.receiver("0x627306090abaB3A6e1400e9345bC60c78a8BEf57");
 
-    final EthTransaction txn = new EthTransaction(txnParams, null, new JsonRpcRequestId(1));
-
+    final EthTransaction txn = new EthTransaction(txnParams, null, null);
     final byte[] serialisedBytes = txn.rlpEncode(chainId);
 
-    CredentialTransactionSigner signer = new CredentialTransactionSigner(
-        Credentials.create("ae6ae8e5ccbfb04590405997ee2d52d2b330726137b875053c36d94e974d162f"));
+    CredentialTransactionSigner signer =
+        new CredentialTransactionSigner(
+            Credentials.create("ae6ae8e5ccbfb04590405997ee2d52d2b330726137b875053c36d94e974d162f"));
 
     final Signature signature = signer.sign(serialisedBytes);
 
     assertThat(signature.getR().signum()).isOne();
     assertThat(signature.getS().signum()).isOne();
   }
-
 }
