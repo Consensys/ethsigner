193768aa9f5b6adf9f95427e8ebae95a928b3e6e
Trent Mohay <trent.mohay@consensys.net>
2020-07-07 12:05:36 +1000

    fixed header helpers test

--- ethsigner/core/src/test/java/tech/pegasys/ethsigner/core/http/HeaderHelpersTest.java
+++ ethsigner/core/src/test/java/tech/pegasys/ethsigner/core/http/HeaderHelpersTest.java
@@ -10,20 +10,7 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package tech.pegasys.ethsigner.core.http; /*
-                                           * Copyright ConsenSys AG.
-                                           *
-                                           * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
-                                           * the License. You may obtain a copy of the License at
-                                           *
-                                           * http://www.apache.org/licenses/LICENSE-2.0
-                                           *
-                                           * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
-                                           * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
-                                           * specific language governing permissions and limitations under the License.
-                                           *
-                                           * SPDX-License-Identifier: Apache-2.0
-                                           */
+package tech.pegasys.ethsigner.core.http;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -40,7 +27,7 @@ class HeaderHelpersTest {
 
   @ParameterizedTest
   @ValueSource(strings = {"ORIGIN", "origin", "Origin", "OrIgIn"})
-  void originHeadersAreRemoved(String headerToRemove) {
+  void originHeaderIsRemoved(String headerToRemove) {
     final MultiMap input = new VertxHttpHeaders();
     input.add(headerToRemove, "arbitrary");
     final Map<String, String> output = HeaderHelpers.createHeaders(input);
@@ -50,7 +37,7 @@ class HeaderHelpersTest {
 
   @ParameterizedTest
   @ValueSource(strings = {"CONTENT-LENGTH", "content-length", "Content-Length", "CoNtEnT-LeNgTh"})
-  void contentLengthHeaderIsStripped(String headerToRemove) {
+  void contentLengthHeaderIsRemoved(String headerToRemove) {
     final MultiMap input = new VertxHttpHeaders();
     input.add(headerToRemove, "arbitrary");
     final Map<String, String> output = HeaderHelpers.createHeaders(input);
@@ -59,7 +46,7 @@ class HeaderHelpersTest {
   }
 
   @Test
-  void hostHeaderIsRenamed() {
+  void hostHeaderIsRenamedToXForwardedHost() {
     final MultiMap input = new VertxHttpHeaders();
     input.add(HttpHeaders.HOST, "arbitrary");
 
