78cd8eeffd3cab17d24eb89491f0f81e862276c1
Usman Saleem <usman@usmans.info>
2020-08-05 23:35:09 +1000

    minor refactoring
    Signed-off-by: Usman Saleem <usman@usmans.info>

--- ethsigner/commandline/src/main/java/tech/pegasys/ethsigner/CommandlineParser.java
+++ ethsigner/commandline/src/main/java/tech/pegasys/ethsigner/CommandlineParser.java
@@ -95,22 +95,20 @@ public class CommandlineParser {
     return resultCode == CommandLine.ExitCode.OK;
   }
 
-  private int executeCommandVersion() {
+  private void executeCommandVersion() {
     final CommandLine baseCommandLine = new CommandLine(baseCommand);
     for (final SignerSubCommand subcommand : signers) {
       baseCommandLine.addSubcommand(subcommand.getCommandName(), subcommand);
     }
     baseCommandLine.printVersionHelp(outputWriter);
-    return baseCommandLine.getCommandSpec().exitCodeOnVersionHelp();
   }
 
-  private int executeCommandUsageHelp() {
+  private void executeCommandUsageHelp() {
     final CommandLine baseCommandLine = new CommandLine(baseCommand);
     for (final SignerSubCommand subcommand : signers) {
       baseCommandLine.addSubcommand(subcommand.getCommandName(), subcommand);
     }
     baseCommandLine.usage(outputWriter);
-    return baseCommandLine.getCommandSpec().exitCodeOnUsageHelp();
   }
 
   private CommandLine.IDefaultValueProvider defaultValueProvider(
