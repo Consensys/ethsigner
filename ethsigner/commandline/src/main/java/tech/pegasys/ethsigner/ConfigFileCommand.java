cfd437c8c28d192786bec4d8e681c1af6be9a562
Usman Saleem <usman@usmans.info>
2020-08-06 10:34:08 +1000

    env variable for config file
    Signed-off-by: Usman Saleem <usman@usmans.info>

--- ethsigner/commandline/src/main/java/tech/pegasys/ethsigner/ConfigFileCommand.java
+++ ethsigner/commandline/src/main/java/tech/pegasys/ethsigner/ConfigFileCommand.java
@@ -17,15 +17,20 @@ import static tech.pegasys.ethsigner.DefaultCommandValues.CONFIG_FILE_OPTION_NAM
 import java.io.File;
 import java.util.List;
 
-import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Unmatched;
 
 // Allows to obtain config file by PicoCLI using two pass approach.
-@CommandLine.Command(mixinStandardHelpOptions = true)
+@Command(mixinStandardHelpOptions = true)
 class ConfigFileCommand {
-  @CommandLine.Option(names = CONFIG_FILE_OPTION_NAME, description = "...")
+  @Option(
+      names = CONFIG_FILE_OPTION_NAME,
+      description = "...",
+      defaultValue = "${env:ETHSIGNER_CONFIG_FILE}")
   File configPath = null;
 
   @SuppressWarnings("UnusedVariable")
-  @CommandLine.Unmatched
+  @Unmatched
   List<String> unmatched;
 }
