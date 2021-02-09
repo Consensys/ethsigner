c6a13c9ec8a9b614ab4cbb9da5e626f96c1cff92
Sally MacFarlane <sally.macfarlane@consensys.net>
2021-02-09 14:38:54 +1000

    correct rpc name
    Signed-off-by: Sally MacFarlane <sally.macfarlane@consensys.net>

--- ethsigner/core/src/test/java/tech/pegasys/ethsigner/core/requesthandler/sendtransaction/transaction/GoQuorumEthTransactionTest.java
+++ ethsigner/core/src/test/java/tech/pegasys/ethsigner/core/requesthandler/sendtransaction/transaction/GoQuorumEthTransactionTest.java
@@ -119,7 +119,7 @@ public class GoQuorumEthTransactionTest {
         "0xf90114a0e04d296d2460cfb8472af2c5fd05b5a214109c25688d3704aed5484f9a7792f28609184e72a0008276c094d46e8dd67c5d32be8058bb8eb970870f0724456704a9d46e8dd67c5d32be8d46e8dd67c5d32be8058bb8eb970870f072445675058bb8eb970870f07244567536a0fe72a92aede764ce41d06b163d28700b58e5ee8bb1af91d9d54979ea3bdb3e7ea046ae10c94c322fa44ddceb86677c2cd6cc17dfbd766924f41d10a244c512996dac5a6c617045736c3971444c50792f6538382b2f36797643554556497648383379304e3441367748754b58493dedac4756386d30565a41636359474141594d42755951744b456a3058747058656177324150636f426d744132773d8a72657374726963746564";
     final JsonRpcRequest jsonRpcRequest = ethTransaction.jsonRpcRequest(transactionString, id);
 
-    assertThat(jsonRpcRequest.getMethod()).isEqualTo("eth_sendRawPrivateTransaction");
+    assertThat(jsonRpcRequest.getMethod()).isEqualTo("goquorum_storeRaw");
     assertThat(jsonRpcRequest.getVersion()).isEqualTo("2.0");
     assertThat(jsonRpcRequest.getId()).isEqualTo(id);
     final List<String> params = (List<String>) jsonRpcRequest.getParams();
